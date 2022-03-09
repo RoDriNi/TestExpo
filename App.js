81c7202a4e3611ecb87834e6d3fc995f66acafe2
rodrini <roni.rodrigues@live.com>
2022-03-09 11:41:16 -0400

    asd

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+  console.log("here parei!");
+  let x=1;
+  //x.toString();
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePressText}>La1 potaria starts here!{x}</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
+const handlePressText = () => {
+  console.log("press");
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
